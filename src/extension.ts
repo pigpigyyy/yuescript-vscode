3aad54ca3c60d837ad5b6cc4504fa155d84fd64b
SkyyySi <53921371+SkyyySi@users.noreply.github.com>
2025-11-02 15:04:21 +0100

    Update - 2025-11-02T15:04:20+01:00

--- src/extension.ts
+++ src/extension.ts
@@ -1,150 +1,126 @@
 import * as vscode from "vscode";
-import { spawn, type ChildProcessWithoutNullStreams } from "node:child_process";
-import { readFile } from "node:fs/promises";
-
-////////////////////////////////////////////////////////////////////////////////
-
-type UTCDateRecord = Readonly<{
-	milliseconds: string;
-	second:       string;
-	minute:       string;
-	hour:         string;
-	day:          string;
-	month:        string;
-	year:         string;
-}>;
-
-function getUTCDateRecord(date?: Date | string | number | null | undefined): UTCDateRecord {
-	date = new Date(date ?? Date.now());
-
-	const zeroPad = (value: number, count: number): string => {
-		return String(value).padStart(count, "0")
-	};
-
-	return {
-		milliseconds: zeroPad(date.getUTCMilliseconds(), 0),
-		second:       zeroPad(date.getUTCSeconds(),      2),
-		minute:       zeroPad(date.getUTCMinutes(),      2),
-		hour:         zeroPad(date.getUTCHours(),        2),
-		day:          zeroPad(date.getUTCDate(),         2),
-		month:        zeroPad(date.getUTCMonth() + 1,    2),
-		year:         zeroPad(date.getUTCFullYear(),     4),
-	} as const;
-}
-
-enum LogLevel {
-	CRITICAL = "CRITICAL",
-	ERROR    = "ERROR",
-	WARNING  = "WARNING",
-	MESSAGE  = "MESSAGE",
-	INFO     = "INFO",
-	VERBOSE  = "VERBOSE",
-	DEBUG    = "DEBUG",
-}
-
-class Logger {
-	readonly #outputChannel: vscode.OutputChannel;
-	#isDisposed: boolean;
-
-	private constructor(
-		outputChannel: vscode.OutputChannel,
-		isDisposed: boolean,
-	) {
-		this.#outputChannel = outputChannel;
-		this.#isDisposed = isDisposed;
-	}
-
-	public static new(context: vscode.ExtensionContext): Logger {
-		const outputChannel: vscode.OutputChannel = vscode
-			.window
-			.createOutputChannel("YueScript");
-		outputChannel.show();
-		context.subscriptions.push(outputChannel);
-
-		return new this(outputChannel, false);
-	}
-
-	public dispose(): void {
-		if (this.#isDisposed) {
-			return;
-		}
-
-		this.#outputChannel.dispose();
-	}
-
-	public [Symbol.dispose](): void {
-		return this.dispose();
-	}
-
-	#writeMessage(message: any, level: LogLevel): void {
-		const dateString: string = ((): string => {
-			const { day, month, hour, minute } = getUTCDateRecord();
-			return `${day}/${month} ${hour}:${minute}`;
-		})();
-
-		const prefix: string = `[YueScript] [${dateString}] [${level}] `;
-		const body: string = `${message}`/*.replaceAll(
-			"\n",
-			"\n" + (" ".repeat(prefix.length)),
-		)*/;
-
-		this.#outputChannel.appendLine(prefix + body);
-	}
-
-	public critical(message: any): void { return this.#writeMessage(message, LogLevel.CRITICAL); }
-	public error   (message: any): void { return this.#writeMessage(message, LogLevel.ERROR); }
-	public warning (message: any): void { return this.#writeMessage(message, LogLevel.WARNING); }
-	public message (message: any): void { return this.#writeMessage(message, LogLevel.MESSAGE); }
-	public info    (message: any): void { return this.#writeMessage(message, LogLevel.INFO); }
-	public verbsoe (message: any): void { return this.#writeMessage(message, LogLevel.VERBOSE); }
-	public debug   (message: any): void { return this.#writeMessage(message, LogLevel.DEBUG); }
-}
-
-let logger: Logger | undefined = undefined;
+import { spawn, type ChildProcessByStdio } from "node:child_process";
+import type { Readable, Writable } from "node:stream";
 
 ////////////////////////////////////////////////////////////////////////////////
 
 export async function activate(context: vscode.ExtensionContext) {
-	logger ??= Logger.new(context);
-	logger.verbsoe("YueScript extension activated!");
-
-	const yueProcess: ChildProcessWithoutNullStreams = spawn("yue", ["-e", context.extensionPath + "/src/server.yue"], {
-		stdio: "pipe",
-		//serialization: "json",
-	});
-
-	yueProcess.stdout.on("data", (data) => {
-		logger?.debug(`stdout: (${typeof data})"${String(data)}"`);
-	});
-
-	yueProcess.stderr.on("data", (data) => {
-		logger?.debug(`stderr: (${typeof data})"${String(data)}"`);
+	const yueProcess: ChildProcessByStdio<Writable, Readable, null> = spawn("yue", ["-e", context.extensionPath + "/src/server.yue"], {
+		stdio: ["pipe", "pipe", "inherit"],
 	});
 
 	context.subscriptions.push({
 		dispose() {
-			yueProcess.stdin?.end();
+			yueProcess.stdin.end();
 			if (!yueProcess.killed) {
 				yueProcess.kill();
 			}
 		}
 	});
 
-	const f = () => {
-		const cb = (...args: any[]) => {
-			logger?.debug(`stdin callback called with args=[${args}]`);
-			setTimeout(f, 1000);
-		};
+	const diagnostics = vscode.languages.createDiagnosticCollection("YueScript");
+	context.subscriptions.push(diagnostics);
 
-		if (!yueProcess.stdin.write(JSON.stringify({
-			sourceCode: "Test"
-		}) + "\r\n")) {
-			yueProcess.stdin.once("drain", cb);
-		} else {
-			process.nextTick(cb);
+	context.subscriptions.push(vscode.workspace.onDidChangeTextDocument(async (event: vscode.TextDocumentChangeEvent) => {
+		const activeEditor = vscode.window.activeTextEditor;
+
+		if ((!activeEditor) || (event.document !== activeEditor.document)) {
+			return;
 		}
-	};
-	f();
+
+		const rawReply = await textChangeCallback({
+			extensionContext: context,
+			activeEditor,
+			yueProcess,
+		});
+		if (rawReply === null) {
+			return;
+		}
+
+		let reply;
+		try {
+			reply = JSON.parse(rawReply);
+		} catch (err) {
+			throw new Error(`${err} -> ${rawReply}`);
+		}
+
+		diagnostics.clear();
+
+		if (!reply || typeof reply !== "object" || reply["success"] || !reply["error"]) {
+			return;
+		}
+
+		const [[node, message, line, column]] = reply["error"];
+		if (node !== "error") {
+			throw new Error(`Invalid node '${node}'!`);
+		}
+		if (typeof message !== "string") {
+			throw new TypeError(`typeof message === '${typeof message}'`);
+		}
+		if (typeof line !== "number") {
+			throw new TypeError(`typeof line === '${typeof line}'`);
+		}
+		if (typeof column !== "number") {
+			throw new TypeError(`typeof column === '${typeof column}'`);
+		}
+
+		diagnostics.set(activeEditor.document.uri, [new vscode.Diagnostic(
+			new vscode.Range(line - 1, column - 1, line - 1, column),
+			message,
+			vscode.DiagnosticSeverity.Error,
+		)]);
+	}));
 }
 
 export function deactivate() {}
+
+////////////////////////////////////////////////////////////////////////////////
+
+function typeName(object: any) {
+	const type = typeof object;
+
+	return type === "object"
+		? (object?.constructor?.name ?? "object")
+		: type;
+}
+
+let locked = false;
+function textChangeCallback({
+	extensionContext,
+	activeEditor,
+	yueProcess,
+}: {
+	extensionContext: vscode.ExtensionContext,
+	activeEditor: vscode.TextEditor,
+	yueProcess: ChildProcessByStdio<Writable, Readable, null>,
+}): Promise<string | null> {
+	return new Promise((resolve, reject) => {
+		if (locked) {
+			resolve(null);
+			return;
+		}
+
+		locked = true;
+
+		const callback = () => {
+			process.nextTick(() => {
+				yueProcess.stdout.once("data", (data: Buffer) => {
+					locked = false;
+					if (data instanceof Buffer) {
+						resolve(String(data));
+					} else {
+						reject(`Invalid type of data! (Buffer expected, got ${typeName(data)} - ${data})`);
+					}
+				});
+			});
+		};
+
+		if (!yueProcess.stdin.write(JSON.stringify({
+			sourceCode: activeEditor.document.getText().trimEnd(),
+		}) + "\n")) {
+			yueProcess.stdin.once("drain", callback);
+		} else {
+			callback();
+		}
+	});
+}
